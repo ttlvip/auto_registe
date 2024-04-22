f7b22e917c06e7256728c23b5b6f3ac38cb2bc80
tlafan <tlafan@vip.qq.com>
2024-04-22 17:49:34 +0800

    初始化项目

--- app.py
+++ app.py
@@ -16,26 +16,16 @@ def register():
     data = request.get_json()  # 获取POST请求的JSON数据
     username = data.get('username')
     password = data.get('password')
-    email = data.get('email')
-
-    # todo 添加代码将数据保存到数据库
-    response = add_user(username, password)
-    if response.status_code == 200:
-        resp_json = response.json()
-        name = resp_json.get("Name")
-        server_id = resp_json.get("ServerId")
-        server_name = resp_json.get("ServerName")
-        user_id = resp_json.get("Id")
-        # 返回一个成功消息
-        return jsonify(
-            {"status": "success",
-             "message": f"Registration successful for username: {name}, server_id: {server_id}, server_name: "
-                        f"{server_name}, id: {user_id}"})
+    resp_message = add_user(username, password)
+    if resp_message.startswith('注册成功'):
+        code = 200
     else:
-        resp_text = response.text
-        resp_text = html.unescape(resp_text)
-        return jsonify(
-            {"status": "fail", "message": f"{resp_text}"})
+        code = 500
+    resp_json = {
+        'code': code,
+        'message': resp_message
+    }
+    return jsonify(resp_json)
 
 
 if __name__ == '__main__':
--- emby/add_user.py
+++ emby/add_user.py
@@ -1,21 +1,44 @@
+import html
 import os
 import json
 import requests
 
-domain = os.environ.get('DOMAIN')
-port = os.environ.get('PORT')
+domain = os.getenv('DOMAIN', 'localhost')
+port = os.environ.get('PORT', '8096')
 api_key = os.environ.get('API_KEY')
 copy_user_id = os.environ.get('COPY_USER_ID')
+isSSL = os.environ.get('IS_SSL', 'N')
 
 
 def add_user(username, password):
-    url = 'http://' + domain + ':' + port + '/emby/Users/New?api_key=' + api_key
-    payload = json.dumps({
-        "Name": username,
-        "CopyFromUserId": copy_user_id
+    prefix = 'https://' if isSSL.lower() == 'y' else 'http://'
+    url = prefix + domain + ':' + port + '/emby/Users/New?api_key=' + api_key
+    new_user_params = json.dumps({
+        'Name': username,
+        'CopyFromUserId': copy_user_id,
+        'UserCopyOptions': ['UserConfiguration', 'UserPolicy']
     })
     headers = {
         'Content-Type': 'application/json'
     }
-    response = requests.request("POST", url, headers=headers, data=payload)
-    return response
+    response = requests.request('POST', url, headers=headers, data=new_user_params)
+    # 创建成功后设置密码
+    if response.status_code == 200:
+        data = response.json()
+        user_id = data.get('Id')
+        url = prefix + domain + ':' + port + '/emby/Users/' + user_id + '/Password?api_key=' + api_key
+        new_password_params = json.dumps({
+            'Id': user_id,
+            'NewPw': password,
+            'ResetPassword': False
+        })
+        response = requests.request('POST', url, headers=headers, data=new_password_params)
+        if response.status_code == 204:
+            return '注册成功! 请使用填写的用户名密码登录.'
+        else:
+            # 如果密码设置失败就删掉创建的用户
+            url = prefix + domain + ':' + port + '/emby/Users/' + user_id + 'Delete?api_key=' + api_key
+            requests.request('POST', url, headers=headers)
+            return '密码设置失败, 请联系管理员'
+    else:
+        return html.unescape(response.text)
